8b76306be8f852a1996388baf740f10f7bd3c970
Prezes <46129802+PanPrezess@users.noreply.github.com>
2020-10-14 00:22:12 +0200

    Update plik1.py

--- lekcja-1/plik1.py
+++ lekcja-1/plik1.py
@@ -1,8 +1,9 @@
 
 # Komentarze są ignorowawne w kodzie
 
-x = 1 # Zmienna typu int
+x = 1 # Zmienna typu integer(int) pełna cyfra
 y = True # Zmienna typu (Przechowuje true/false)
-z = "Test" # Zmienna typu string
+z = "Test" # Zmienna typu string(zdanie)
 
 a = 1.2 # Zmienna typu float
+b = 'g' # Zmienna typu char - litera
