1a456f81753829d9b47b02370fe4caa349f5214b
Prezes <46129802+PanPrezess@users.noreply.github.com>
2020-10-14 00:24:46 +0200

    Update plik2.py

--- lekcja-1/plik2.py
+++ lekcja-1/plik2.py
@@ -1,4 +1,4 @@
 
-x = False
+x = False # Tworzy zmienną x
 
-print("Wypisz coś w konsoli " + str(x))
+print("Wypisz coś w konsoli " + str(x)) # wypisuje False str() służy do zmiany zmiennej takiej jak np. boolean na string
