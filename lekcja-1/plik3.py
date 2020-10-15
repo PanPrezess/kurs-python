fd751882179b57da83367ab80bf552c382aa8ffd
Prezes <46129802+PanPrezess@users.noreply.github.com>
2020-10-15 09:26:47 +0200

    Update plik3.py

--- lekcja-1/plik3.py
+++ lekcja-1/plik3.py
@@ -1,7 +1,7 @@
 
 a = True # Tworzy zmienną a przechowującą True
 
-print(a) # Wypisuję zmienną(nie trzeba zmieniać na string ponieważ nie ma obok niej typu string)
+print(a) # Wypisuję zmienną(nie trzeba zmieniać na string ponieważ nie ma obok niej typu string!)
 
 a = False # Zmienie zmienną na False
 
