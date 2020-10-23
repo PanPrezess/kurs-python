f39f51cad2febd0dfb0efe003985f7975e2ba6c7
Prezes <46129802+PanPrezess@users.noreply.github.com>
2020-10-23 17:15:39 +0200

    Add files via upload

---
+++ lekcja-3/plik13.py
@@ -0,0 +1,7 @@
+
+def hejka(imie, pytanie):
+    print("Dzień dobry " + imie + pytanie)
+
+hejka("Bartek", "która godzina?")
+hejka("Wojtek", "pożyczysz grosika?")
+hejka("Maciek", "gdzie jest sklep?")
